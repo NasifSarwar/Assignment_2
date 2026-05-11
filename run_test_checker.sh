657951687e5b0c5e06e167d5e9e20c786fcdc2ba
Yeabkalu Merkebe <ymerkebe@r18c04.bullx>
2026-05-11 08:30:00 +0300

    checker slurm job

---
+++ run_test_checker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=cuda_renderer
+#SBATCH --account=project_2018477
+#SBATCH --partition=gputest
+#SBATCH --time=00:15:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem=16G
+
+module load gcc/13.2.0
+module load cuda/12.6.0
+module load python-data/3.12-25.09
+
+export LD_LIBRARY_PATH=$(dirname $(which nvcc))/../lib64:$LD_LIBRARY_PATH
+
+cd render
+make
+./checker.py
